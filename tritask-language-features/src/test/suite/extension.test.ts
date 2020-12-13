2df5f0e1c9d87c44baa7d937e12b29b13f2e51b4
stakiran <stakiran@users.noreply.github.com>
2020-12-13 14:43:02 +0900

    add 非同期テストの要領たぶん理解, コメントも拡充

--- tritask-language-features/src/test/suite/extension.test.ts
+++ tritask-language-features/src/test/suite/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import { before, after, beforeEach, afterEach } from 'mocha';
+import { before, after } from 'mocha';
 
 import * as vscode from 'vscode';
 
@@ -24,12 +24,29 @@ class IDE {
 	}
 }
 
+// ここで採用している mocha + 非同期テストの書き方.
+// - suite と test を使う.
+//   - suite は describe, test は it の糖衣構文(たぶん)
+//   - わかりやすいネーミングだと思うので採用
+// - before や test の中では promise object を返す.
+//   - これにより, (当該の処理が終わるまで)次の test に行くのを待たせることができる
+//   - (mocha 側が await 的なのを頑張ってくれてる)
+// - 必要なデータは TestData でやりとりする.
+//   - 前の test で return した promise object を, 次の test は受け取れないため.
+// - テストコードの(非同期処理の)終わりでは done() する
+//   - これしないと Error: Timeout of 2000ms exceeded. になる
+//   - done とは test(), もっというと it() に渡されてくる「非同期処理終わったならこれ呼んでね」関数
+
+class TestData {
+	static document: vscode.TextDocument;
+}
+
 suite('describe1', () => {
 	before(() => {
 		const promise = IDE.openTesteeFile()
 		return promise.then(
 			(doc: vscode.TextDocument) => {
-				console.log(doc)
+				TestData.document = doc
 			},
 			(err) => {
 				console.log(err)
@@ -41,14 +58,15 @@ suite('describe1', () => {
 	after(() => {
 	});
 
-	beforeEach(() => {
-	});
-
-	afterEach(() => {
-	});
-
 	test('test1', () => {
 		assert.strictEqual(1, 100-98-1);
+		console.log(TestData.document)
+	});
+
+	test('test2', (done) => {
+		assert.strictEqual(1, 100-98-1);
+		console.log(TestData.document.fileName)
+		done()
 	});
 
 });
