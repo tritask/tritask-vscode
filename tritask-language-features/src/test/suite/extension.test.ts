f148b581e388a07f9d4c01ff6da8234e14e8b4ab
stakiran <stakiran@users.noreply.github.com>
2020-12-13 13:12:31 +0900

    add getting current directory system.

--- tritask-language-features/src/test/suite/extension.test.ts
+++ tritask-language-features/src/test/suite/extension.test.ts
@@ -3,41 +3,60 @@ import { before, after, beforeEach, afterEach } from 'mocha';
 
 import * as vscode from 'vscode';
 
-// import * as myExtension from '../extension';
+import * as path from 'path'
 
-class IDE {
-	static showInfo(msg: string){
-		vscode.window.showInformationMessage(msg);
+class Path {
+	static get _entrypoint(){
+		// __dirname には現在実行中のソースコードが格納されているディレクトリ.
+		// node.js の動作.
+		// src/test/suite/test.ts の場合, out/test/suite になる
+		return path.resolve(__dirname)
 	}
 
-	static openFile(){
+	static get root(){
+		const defaultRoot = this._entrypoint
+		return path.resolve(defaultRoot, '..', '..', '..', 'src', 'test', 'suite')
+	}
+}
+
+class IDE {
+	static openTesteeFile(){
+		const TESTEE_FILENAME = 'test.trita'
+		const TESTEE_FULLLPATH = path.resolve(Path.root, TESTEE_FILENAME)
+		return vscode.workspace.openTextDocument(TESTEE_FULLLPATH)
+	}
+
+	static getEditor(){
+		const editor = vscode.window.activeTextEditor;
+		if(editor == null){
+			throw new Error("activeTextEditor is null currently.");
+		}
+		return editor;
 	}
 }
 
 suite('describe1', () => {
 	before(() => {
-		IDE.showInfo('Run all tests.')
-		console.log('before')
+		console.log(Path.root)
+		const promise = IDE.openTesteeFile()
+		promise.then(
+			(doc: vscode.TextDocument) => {
+				console.log(doc)
+			}
+		)
 	});
 
 	after(() => {
-		console.log('after')
 	});
 
 	beforeEach(() => {
-		console.log('before')
 	});
 
 	afterEach(() => {
-		console.log('after')
 	});
 
 	test('test1', () => {
 		assert.strictEqual(1, 100-98-1);
-	});
-
-	test('test2', () => {
-		assert.strictEqual(2, 100-98);
 	});
 
 });
