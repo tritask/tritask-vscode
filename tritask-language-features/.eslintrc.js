a74514a7c19a66c786cf51a508c3dac3060c86bd
stakiran <stakiran@users.noreply.github.com>
2020-12-15 20:17:59 +0900

    turn off about explicit-module-boundary-types

--- tritask-language-features/.eslintrc.js
+++ tritask-language-features/.eslintrc.js
@@ -8,5 +8,9 @@ module.exports = {
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
     ],
+    rules: {
+      // 利用者(export先)はテストコードだけで, 正直面倒くさいので切る
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
   };
   
